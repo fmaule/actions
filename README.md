f6b2ca8a2416da353c3fad79af27b2c49475a689
Fernando Maule <fer@maule.pro>
2022-03-08 20:43:02 +0100

    feat: add base and template

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # actions
-A collection of Github actions
+
+A collection of Github actions.
+
+To create a new action: `pnpm run new -- --name new-action`
+
+Uses [pnpm](https://pnpm.io/) 🚀
---
+++ create-action.js
@@ -0,0 +1,34 @@
+const { program } = require('commander');
+const shell = require("shelljs");
+const fs = require('fs').promises
+const yaml = require('js-yaml')
+
+const main = async () => {
+  program.option('--name <string>')
+  program.parse();
+  const { name } = program.opts();
+
+  if (!name) throw new Error('Name is required :#')
+
+  console.log(`Creating ${name}`)
+  const destinationDir = `./${name}`
+  shell.mkdir('-p', destinationDir)
+  shell.cp('-R', '_template/*', destinationDir)
+
+  const packageJsonFile = `${destinationDir}/package.json`
+  const packageJson = require(packageJsonFile)
+  const newPackageJson = { ...packageJson, name }
+  fs.writeFile(packageJsonFile, JSON.stringify(newPackageJson, null, 2))
+
+  const workspaceYamlFile = './pnpm-workspace.yaml'
+  const workspaceYaml = await fs.readFile(workspaceYamlFile, 'utf8')
+  const workspace = yaml.load(workspaceYaml)
+  const newWorkspace = { packages: [...workspace.packages || [], `${name}/`] }
+  fs.writeFile(workspaceYamlFile, yaml.dump(newWorkspace))
+
+  shell.exec(`pnpm --filter ${destinationDir} install`)
+
+  console.log('🎉 DONE!')
+}
+
+main().catch(err => console.error(err))
