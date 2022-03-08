fcbbe4b5c99018caebbeac021c5c8f84bbe8c74d
Fernando Maule <fer@maule.pro>
2022-03-08 22:58:24 +0100

    add action and (test?) autobuild workflow

--- inject-helm-secrets/src/index.js
+++ inject-helm-secrets/src/index.js
@@ -1,12 +1,42 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require('fs').promises
+const path = require('path')
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+const escapeRegExp = (string) => string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
+  
+const writeFile = async (filePath, data) => {
+  await fs.mkdir(path.dirname(filePath), { recursive: true })
+  await fs.writeFile(filePath, data)
+}
+
+const setOutputResult = async (result) => {
+  core.setOutput('result', result)
+
+  const outputFile = core.getInput('_output-file')
+
+  if (outputFile) {
+    await writeFile(outputFile, result)
+    core.info(`Injected secrets into ${outputFile}`)
+  }
+}
 
 const main = async () => {
-    const foo = core.getInput('foo');
-    core.setOutput('bar', foo);
+  const inputFile = core.getInput('_input-file')
+  const secrets = JSON.parse(core.getInput('_secrets-json'))
+  const formatKey = core.getInput('_format-key')
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+  let secretsYaml = await fs.readFile(inputFile, 'utf8')
+
+  for (const [key, value] of Object.entries(secrets)) {
+    const newKey = formatKey.replace('key', key)
+    const pattern = new RegExp(escapeRegExp(newKey), 'gi')
+    // NOTE: Replace with value in function syntax to prevent special patterns replacement
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_string_as_a_parameter
+    secretsYaml = secretsYaml.replace(pattern, () => value)
+  }
+  
+  await setOutputResult(secretsYaml)
 }
 
 main().catch(err => core.setFailed(err.message))
